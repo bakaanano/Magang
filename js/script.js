ede1bf1a5f65a6a1e8cb2940496ce5b0be161a3d
TaufiqAndriono <taufiqandriono688@gmail.com>
2024-03-26 17:25:08 +0700

    update tugas 2 card-task

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let menu_makanan = ["Nasi Pecel" , 
                     "KFC" , 
-                    "Nasi Rawon"];
+                    "Nasi Rawon" ,];
 const makanan1 = 10000;
 const makanan2 = 25000;
 const makanan3 = 15000;
@@ -9,4 +9,25 @@ let diskon = 0.1 * total ;
 let totalpembayaran = total - diskon;
 //document.getElementById('demo').innerHTML = "A. " + menu_makanan;
 document.getElementById('demo1').innerHTML = "B. Total Pembayaran = " + total;
-document.getElementById('demo2').innerHTML = "C. diskon 10% menjadi =" + totalpembayaran;   
+document.getElementById('demo2').innerHTML = "C. Diskon 10% menjadi =" + totalpembayaran;   
+
+
+let makanan_menu = [" Es Kopi Susu : Rp. 10000", 
+                    " Roti Bakar : Rp. 25000",
+                    " Ice Cream : Rp. 15000",
+                    " Nasi Goreng : Rp. 12000"];
+const harga1 = 10000;
+const harga2 = 25000;
+const harga3 = 15000;
+const harga4 = 12000;
+let hargaawal = harga1 + harga2 + harga3 + harga4;
+let disc = 0.1 * hargaawal ;
+let hargaakhir = hargaawal - disc;
+let Menu = "";
+for (let i = 0; i < makanan_menu.length; i++) {
+    Menu +=`<li>${makanan_menu[i]}</li><br>`;
+}
+document.getElementById('tes').innerHTML = Menu;
+document.getElementById('tes1').innerHTML = "B. Total Pembayaran = " + hargaawal;
+document.getElementById('tes2').innerHTML = "C. Diskon 10% menjadi =" + hargaakhir;
+
