2e10a3a8b7391c8f3dd6e4d1942a150f2a57e02a
TaufiqAndriono <taufiqandriono688@gmail.com>
2024-03-26 12:45:01 +0700

    update card-task

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
+let menu_makanan = ["Nasi Pecel" , 
+                    "KFC" , 
+                    "Nasi Rawon"];
 const makanan1 = 10000;
 const makanan2 = 25000;
 const makanan3 = 15000;
 let total = makanan1 + makanan2 + makanan3;
-let totalpembayaran = total - (10/100 * total);
-document.getElementById('demo').innerHTML = "A. " + harga_makanan;
+let diskon = 0.1 * total ;
+let totalpembayaran = total - diskon;
+//document.getElementById('demo').innerHTML = "A. " + menu_makanan;
 document.getElementById('demo1').innerHTML = "B. Total Pembayaran = " + total;
 document.getElementById('demo2').innerHTML = "C. diskon 10% menjadi =" + totalpembayaran;   
