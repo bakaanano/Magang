e8d7ad60430e568e47f387058eb95511a333bd7e
TaufiqAndriono <taufiqandriono688@gmail.com>
2024-03-27 11:41:13 +0700

    done

--- js/script.js
+++ js/script.js
@@ -34,3 +34,32 @@ document.getElementById('tes').innerHTML = Menu;
 document.getElementById('tes1').innerHTML = "B. Total Pembayaran = " + hargaawal;
 document.getElementById('tes2').innerHTML = "C. Diskon 10% menjadi =" + hargaakhir;
 
+
+/*let mm = [" Es Kopi Susu : Rp. 10000", 
+            " Roti Bakar : Rp. 25000",
+            " Ice Cream : Rp. 15000",
+            " Nasi Goreng : Rp. 12000",
+            "Ayam Goreng : Rp. 20000"];
+*/
+
+let lm = [
+    {'Menu': 'Es Kopi Susu', 'harga' : 'Rp. 10.000'},
+    {'Menu': 'Roti Bakar', 'harga' : 'Rp. 25000'},
+    {'Menu': 'Ice Cream', 'harga' : 'Rp. 15000'},
+    {'Menu': 'Nasi Goreng', 'harga': 'Rp. 12000'}
+]
+const price1 = 10000;
+const price2 = 25000;
+const price3 = 15000;
+const price4 = 12000;
+let priceA = price1 + price2 + price3 + price4;
+let kondis = 0.1 * priceA;
+let priceB = priceA - kondis;
+let list = "";
+for (let i = 0; i < lm.length; i++) {
+    list +=`<li>${("Menu :" + lm[i]['Menu'] + ", Harga: " + lm[i]['harga'])}</li><br>`; 
+}
+document.getElementById('ob').innerHTML = list;
+document.getElementById('ob1').innerHTML = "B. Total Pembayaran = " + priceA;
+document.getElementById('ob2').innerHTML = "C. Diskon 10% menjadi =" + priceB;
+
